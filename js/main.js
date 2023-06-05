c8f72e9221e7ad008f2ffa36e2de9347bf045f6d
mr_dwnial <dwnial_11fnb@yahoo.com>
2023-06-05 15:29:26 +0330

    fix scroll onload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-window.onload = function () {
-  setTimeout(function () {
-    window.scrollTo(0, 1);
+window.addEventListener("load",function() {
+  setTimeout(function(){
+      // This hides the address bar:
+      window.scrollTo(0, 1);
   }, 0);
-};
+});
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
